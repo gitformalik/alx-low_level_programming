d3e660345fe5654e54d464c348d120dda4914b7f
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-03 16:51:34 +0300

    Update 101-quote.c
    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,21 @@
-#include <stdlib.h>
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <unistd.h>
+
 /**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- *  * main - Entry point
- *
- *   * Return: Always 1 (Success)
- *
- *    */
-
+ * Return: Always 1.
+ */
 int main(void)
-
 {
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 
-		write(STDOUT_FILENO, "and that piece of art is useful
-
-							\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
+	return (1);
 }
