5e7226e070051d17dfe09378090d2a6c1599eadd
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-03 16:47:51 +0300

    Update 5-print_numbers.c
    corrections

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,23 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
+
 /**
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
- *  * main - prints all single digit numbers of base
- *
- *   * 10 starting from 0, followed by a new line
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0.
+ */
 int main(void)
 {
-		int n;
-for (n = 0; n < 10; n++)
-	printf("%d", n);
-	printf("\n");
-	return (0);
+	int num;
 
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
 }
