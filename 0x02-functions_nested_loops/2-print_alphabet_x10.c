2993a27c4e95293567976444f9fcb01858665c85
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-05 17:32:13 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lower
- *
  * Return : Always return 0
- *
 */
 void print_alphabet_x10(void)
 {
@@ -17,7 +15,7 @@ while (ch <= 'z')
 {
 _putchar(ch);
 ch++;
-} 
+}
 _putchar('\n');
 i++;
 }
