3dd59abb4749a0dcbe6a5161d4ab64653328d2fb
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-05 15:41:39 +0300

    Betty Corrections

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,29 +1,15 @@
 #include "main.h"
-
 /**
+ * add - adds two integrers and returns the result
  *
- *  * add - adds two integrers and returns the result
+ * @x: number being added
  *
- *   *
+ * @y: number being added
  *
- *    * @x: number being added
+ * Return: Always 0.
  *
- *     *
- *
- *      * @y: number being added
- *
- *       *
- *
- *        * Return: Always 0.
- *
- *         *
- *
- *          */
-
+*/
 int add(int x, int y)
-
 {
-
-		return (x + y);
-
+	return (x + y);
 }
