1ea06a520c2be2998a6f369f6bf342faa90b09db
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-07 13:36:02 +0300

    Betty corrections-2nd

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,57 +1,29 @@
 #include "main.h"
-
-
-
 /**
+ * print_diagonal - draws a diagonal line on the terminal
  *
- * * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times \ should be printed
  *
- * * @n: number of times \ should be printed
+ * Return: no return
  *
- * *
- *
- * * Return: no return
- *
- * */
-
+*/
 void print_diagonal(int n)
-
 {
-
-		int i, j;
-
-
-
-			if (n > 0)
-
-					{
-
-								for (i = 0; i < n; i++)
-
-											{
-
-															for (j = 0; j < i; j++)
-
-																			{
-
-																								_putchar(' ');
-
-																											}
-
-																		_putchar('\\');
-
-																					_putchar('\n');
-
-																							}
-
-									}
-
-				else if (n <= 0)
-
-						{
-
-									_putchar('\n');
-
-										}
-
+int i, j;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else if (n <= 0)
+{
+_putchar('\n');
+}
 }
