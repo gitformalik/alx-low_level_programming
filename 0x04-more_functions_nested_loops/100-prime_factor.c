62581f02fdbc93c45a5a7b194285d70ae67c368c
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-07 17:06:50 +0300

    corrections on Betty

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,53 +1,23 @@
 #include <stdio.h>
-
 #include <math.h>
-
-
-
 /**
+ * main - Prints the largest prime factor of the number 612852475143
  *
- *  * main - Prints the largest prime factor of the number 612852475143
+ * Return: Alway 0 (Success)
  *
- *   *
- *
- *    * Return: Alway 0 (Success)
- *
- *     */
-
-
-
+*/
 int main(void)
-
 {
-
-		long a, maxf;
-
-			long number = 612852475143;
-
-				double square = sqrt(number);
-
-
-
-					for (a = 1; a <= square; a++)
-
-							{
-
-										if (number % a == 0)
-
-													{
-
-																	maxf = number / a;
-
-																			}
-
-											}
-
-
-
-						printf("%ld\n", maxf);
-
-
-
-							return (0);
-
+long a, maxf;
+long number = 612852475143;
+double square = sqrt(number);
+for (a = 1; a <= square; a++)
+{
+if (number % a == 0)
+{
+maxf = number / a;
+}
+}
+printf("%ld\n", maxf);
+return (0);
 }
