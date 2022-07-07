cc2028d292e21285a122f4767875bbcd6ae4bfcd
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-07 11:18:03 +0300

    betty corrections

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,29 +1,16 @@
 #include "main.h"
-
-
-
 /**
+ * _isupper - Checks if c is uppercase character
  *
- * * _isupper - Checks if c is uppercase character
+ * @c: Character to be checked
  *
- * * @c: Character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  *
- * *
- *
- * * Return: 1 if c is uppercase, 0 otherwise
- *
- * */
-
+*/
 int _isupper(int c)
-
 {
-
-		if (c >= 'A' && c <= 'Z')
-
-					return (1);
-
-			else
-
-						return (0);
-
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
 }
