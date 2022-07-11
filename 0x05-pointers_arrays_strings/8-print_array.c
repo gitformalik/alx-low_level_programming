03be43a2b53458b46862ae1475fe910d30ff42aa
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-12 01:34:51 +0300

    betty

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,43 +1,23 @@
 #include "main.h"
-
 #include <stdio.h>
-
 /**
+ * print_array - prints n times the elements of an array
  *
- *  * print_array - prints n times the elements of an array
+ * @a: the pointer to the elements
  *
- *   * @a: the pointer to the elements
+ * @n: times to print elements
  *
- *    * @n: times to print elements
- *
- *     */
-
+*/
 void print_array(int *a, int n)
-
 {
-
-		int i;
-
-
-
-			for (i = 0 ; i < n ; i++)
-
-					{
-
-								printf("%d", a[i]);
-
-
-
-										if (i < n - 1)
-
-													{
-
-																	printf(", ");
-
-																			}
-
-											}
-
-				putchar('\n');
-
+int i;
+for (i = 0 ; i < n ; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+putchar('\n');
 }
