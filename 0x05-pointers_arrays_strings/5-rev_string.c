1d32f9526c5f1278a213b75bb468d1b3ccb77c48
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-12 02:05:08 +0300

    betty code corrections

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,23 @@
-#include "main.h"
 /**
- *
- * rev_string - reverses a string
- *
- * @s: holi
- *
-*/
+ * rev_string - reverses a string.
+ * @s: input string to reverse.
+ */
 void rev_string(char *s)
 {
-int i;
-int j;
-int temp;
-int length;
-while (s[i] != 0)
-{
-i++;
-}
-length = i;
-i = 0;
-j = length - 1;
-while (i < j)
-{
-temp = s[i];
-s[i] = s[j];
-s[j] = temp;
-i++;
-j--;
-}
+	int i, j;
+	char r[1000];
+	i = j = 0;
+	while (s[i] != '\0')
+	{
+		r[i] = s[i];
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		s[i] = r[j];
+		i--;
+		j++;
+	}
+	s[j++] = '\0';
 }
