81186fdc3f8514273c2bd27db62a53f85f50b9b6
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-12 01:24:14 +0300

    betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,43 +1,23 @@
 #include "main.h"
-
 /**
+ * print_rev -prints a string in reverse
  *
- *  * print_rev -prints a string in reverse
+ * @s: a string
  *
- *   * @s: a string
+ * Return: string in reverse
  *
- *    *
- *
- *     * Return: string in reverse
- *
- *      */
-
+*/
 void print_rev(char *s)
-
 {
-
-		int i = 0;
-
-
-
-			while (s[i] != 0)
-
-					{
-
-								i++;
-
-									}
-
-				while (i > 0)
-
-						{
-
-									i--;
-
-											_putchar(s[i]);
-
-												}
-
-					_putchar('\n');
-
+int i = 0;
+while (s[i] != 0)
+{
+i++;
+}
+while (i > 0)
+{
+i--;
+_putchar(s[i]);
+}
+_putchar('\n');
 }
