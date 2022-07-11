0cea1da8f8fc74c8e19e8bca23d353328cb63353
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-12 01:19:20 +0300

    betty correc

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,27 +1,15 @@
 #include "main.h"
-
 /**
+ * swap_int - swaps the values of two integers
  *
- *  * swap_int - swaps the values of two integers
+ * @a: integer 1
  *
- *   * @a: integer 1
+ * @b: integer 2
  *
- *    * @b: integer 2
- *
- *     *
- *
- *      **/
-
+*/
 void swap_int(int *a, int *b)
-
 {
-
-		int c = *a;
-
-
-
-			*a = *b;
-
-				*b = c;
-
+int c = *a;
+*a = *b;
+*b = c;
 }
