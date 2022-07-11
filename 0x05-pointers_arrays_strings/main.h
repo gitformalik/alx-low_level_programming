f56df31830f4721c6f8999815aad7889331d85c2
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-12 01:55:02 +0300

    correct

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,5 @@
-fndef MAIN_H
-#define MAIN_H
-void reset_to_98(int *n);
 int _putchar(char c);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -11,4 +9,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-#endif
+int _atoi(char *s);
