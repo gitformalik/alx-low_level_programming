94f898ad74990e0552feaa69bed32465d88ca543
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-12 01:17:29 +0300

    betty corre

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,13 @@
 #include "main.h"
-
 /**
+ * reset_to_98 - takes a pointer to an int as parameter
  *
- *  * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to 98
  *
- *   * and updates the value it points to 98
+ * @n: parameter to take the value.
  *
- *    * @n: parameter to take the value.
- *
- *     *
- *
- *      */
-
+*/
 void reset_to_98(int *n)
-
 {
-
-		*n = 98;
-
+*n = 98;
 }
