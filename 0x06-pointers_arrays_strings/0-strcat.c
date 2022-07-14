e1614a91976ed39e2e5859fc2294b467eaee8225
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-14 22:30:46 +0200

    corrections

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,59 +1,29 @@
 #include "main.h"
-
 /**
+ *_strcat - conctenates two strings
  *
- *  *_strcat - conctenates two strings
+ *@dest:destination
  *
- *   *
+ *@src:source
  *
- *    *@dest:destination
+ *Return:char
  *
- *     *@src:source
- *
- *      *
- *
- *       *Return:char
- *
- *        *
- *
- *         */
-
+*/
 char *_strcat(char *dest, char *src)
-
 {
-
-		int i, j;
-
-
-
-			i = 0;
-
-				j = 0;
-
-
-
-					while (dest[i] != '\0')
-
-							{
-
-										i++;
-
-											}
-
-						while (src[j] != '\0')
-
-								{
-
-											dest[i] = src[j];
-
-													j++;
-
-															i++;
-
-																}
-
-							dest[i] = '\0';
-
-								return (dest);
-
+int i, j;
+i = 0;
+j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
 }
