265e0b3981f1f04f7c66491f2699df04c599ed59
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-14 22:55:38 +0200

    betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,47 +1,26 @@
 /**
+ *_strcmp - compares two strings
  *
- *  *_strcmp - compares two strings
+ *@s1:string 1
  *
- *   *
+ *@s2:string 2
  *
- *    *@s1:string 1
+ *Return:0- same,-ve - less ,+ve -more
  *
- *     *@s2:string 2
- *
- *      *
- *
- *       *Return:0- same,-ve - less ,+ve -more
- *
- *        */
-
+*/
 int _strcmp(char *s1, char *s2)
-
 {
-
-		while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
-
-				{
-
-							s1++;
-
-									s2++;
-
-										}
-
-			if (*s1 == *s2)
-
-					{
-
-								return (0);
-
-									}
-
-				else
-
-						{
-
-									return (*s1 - *s2);
-
-										}
-
+while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+if (*s1 == *s2)
+{
+return (0);
+}
+else
+{
+return (*s1 - *s2);
+}
 }
