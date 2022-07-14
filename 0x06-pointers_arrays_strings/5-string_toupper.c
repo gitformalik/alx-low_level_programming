cc43abf0fb440a74ffe550f555c5983236920dc2
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-14 23:03:08 +0200

    betty

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,39 +1,21 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
+ *string_toupper - characters to uppercase
  *
- *  *string_toupper - characters to uppercase
+ *@s:input
  *
- *   *
+ *Return:pointer of string
  *
- *    *@s:input
- *
- *     *Return:pointer of string
- *
- *      */
-
+*/
 char *string_toupper(char *s)
-
 {
-
-		int i;
-
-
-
-			for (i = 0; s[i] != '\0'; i++)
-
-					{
-
-								if (s[i] >= 'a' && s[i] <= 'z')
-
-											{
-
-															s[i] -= 32;
-
-																	}
-
-									}
-
-				return (s);
-
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] -= 32;
+}
+}
+return (s);
 }
