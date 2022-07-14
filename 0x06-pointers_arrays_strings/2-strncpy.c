a0648939d8fd083c2ebe31dbdbe877b086d22997
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-14 22:52:02 +0200

    betty

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,49 +1,26 @@
 #include "main.h"
-
 /**
+ *_strncpy - copies a string
  *
- *  *_strncpy - copies a string
+ *@dest:destination
  *
- *   *
+ *@src:source
  *
- *    *@dest:destination
+ *@n:range
  *
- *     *@src:source
+ *Return:dest
  *
- *      *@n:range
- *
- *       *
- *
- *        *Return:dest
- *
- *         *
- *
- *          */
-
+*/
 char *_strncpy(char *dest, char *src, int n)
-
 {
-
-		int i;
-
-
-
-			for (i = 0; src[i] != '\0' && i < n; i++)
-
-					{
-
-								dest[i] = src[i];
-
-									}
-
-				for (; i < n; i++)
-
-						{
-
-									dest[i] = '\0';
-
-										}
-
-					return (dest);
-
+int i;
+for (i = 0; src[i] != '\0' && i < n; i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
