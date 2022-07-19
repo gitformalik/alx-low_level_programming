7c53d60b451a7a57514c390b331c3322dc1d8494
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-19 19:02:29 +0300

    betty

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,65 +1,33 @@
 #include "main.h"
-
-
-
 /**
+ * _strstr - finds the first occurrence of the substring
  *
- *  * _strstr - finds the first occurrence of the substring
+ * needle in the string haystack
  *
- *   * needle in the string haystack
+ * @haystack: the string to be scanned
  *
- *    * @haystack: the string to be scanned
+ * @needle: substring to match
  *
- *     * @needle: substring to match
+ * Return: pointer to the beginning of the located
  *
- *      *
+ * substring, NULL if not found.
  *
- *       * Return: pointer to the beginning of the located
- *
- *        * substring, NULL if not found.
- *
- *         */
-
+*/
 char *_strstr(char *haystack, char *needle)
-
 {
-
-		char *str1, *pattern;
-
-
-
-			while (*haystack)
-
-					{
-
-								str1 = haystack;
-
-										pattern = needle;
-
-
-
-												while (*haystack && *pattern && *haystack == *pattern)
-
-															{
-
-																			haystack++;
-
-																						pattern++;
-
-																								}
-
-
-
-														if (!*pattern)
-
-																		return (str1);
-
-
-
-																haystack = str1 + 1;
-
-																	}
-
-				return (0);
-
+char *str1, *pattern;
+while (*haystack)
+{
+str1 = haystack;
+pattern = needle;
+while (*haystack && *pattern && *haystack == *pattern)
+{
+haystack++;
+pattern++;
+}
+if (!*pattern)
+return (str1);
+haystack = str1 + 1;
+}
+return (0);
 }
