e56da911b21947622c0bbb29a5182eec86ad301e
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-19 19:29:32 +0300

    betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,35 +1,20 @@
 #include "main.h"
-
-
-
 /**
+ * _memset - it fills a block of memory with constant byte.
  *
- *  * _memset - it fills a block of memory with constant byte.
+ * @s: a pointer to a memory area.
  *
- *   * @s: a pointer to a memory area.
+ * @b:the constant byte.
  *
- *    * @b:the constant byte.
+ * @n: the number of bytes of memory to be filled with b.
  *
- *     * @n: the number of bytes of memory to be filled with b.
+ * Return: s.
  *
- *      *
- *
- *       * Return: s.
- *
- *        */
-
+*/
 char *_memset(char *s, char b, unsigned int n)
-
 {
-
-		unsigned int i;
-
-
-
-			for (i = 0; i < n; i++)
-
-						s[i] = b;
-
-				return (s);
-
+unsigned int i;
+for (i = 0; i < n; i++)
+s[i] = b;
+return (s);
 }
