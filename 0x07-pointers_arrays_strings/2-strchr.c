0eb14d3da713736d1d17d28926371739fa60eb60
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-19 18:57:05 +0300

    betty

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,31 +1,18 @@
 #include "main.h"
-
-
-
 /**
+ * _strchr - locates a character in a string.
  *
- *  * _strchr - locates a character in a string.
+ * @s: the string to search through.
  *
- *   * @s: the string to search through.
+ * @c: the character to be searched for.
  *
- *    * @c: the character to be searched for.
+ * Return: a pointer to the first occurrence of c in s.
  *
- *     *
- *
- *      * Return: a pointer to the first occurrence of c in s.
- *
- *       */
-
+*/
 char *_strchr(char *s, char c)
-
 {
-
-		while (*s != c)
-
-					if (!*s++)
-
-									return (0);
-
-			return (s);
-
+while (*s != c)
+if (!*s++)
+return (0);
+return (s);
 }
