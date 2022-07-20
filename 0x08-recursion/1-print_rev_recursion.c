78929d0e694322b6c4c7f0f29434a0cff94d08d7
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-20 16:12:28 +0300

    betty

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,31 +1,17 @@
-#include "holberton.h"
-
-
-
+#include "main.h"
 /**
+ * _print_rev_recursion - prints a string in reverse.
  *
- *  * _print_rev_recursion - prints a string in reverse.
+ * @s: the string to operate on.
  *
- *   * @s: the string to operate on.
+ * Return: nothing
  *
- *    *
- *
- *     * Return: nothing
- *
- *      */
-
+*/
 void _print_rev_recursion(char *s)
-
 {
-
-		if (*s != '\0')
-
-				{
-
-							_print_rev_recursion(s + 1);
-
-									_putchar(*s);
-
-										}
-
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
 }
