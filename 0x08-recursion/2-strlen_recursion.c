99dc79c8c36533fec16555038fe18fc64e6d93e7
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-20 16:18:43 +0300

    betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,29 +1,16 @@
 #include "holberton.h"
-
-
-
 /**
+ * _strlen_recursion - returns the length of a string
  *
- *  * _strlen_recursion - returns the length of a string
+ * @s: the string whose length is to be calculated.
  *
- *   * @s: the string whose length is to be calculated.
+ * Return: the length of s.
  *
- *    *
- *
- *     * Return: the length of s.
- *
- *      */
-
+*/
 int _strlen_recursion(char *s)
-
 {
-
-		if (*s == '\0')
-
-					return (0);
-
-			else
-
-						return (1 + _strlen_recursion(s + 1));
-
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
