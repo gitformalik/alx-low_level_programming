15dac2ffa93ed5949c66096a653b3ac8757fdd8e
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-20 17:41:45 +0300

    betty

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,63 +1,35 @@
-#include "holberton.h"
-
-
-
+#include "main.h"
 /**
+ * is_prime_number - checks if the input integer is a prime number.
  *
- *  * is_prime_number - checks if the input integer is a prime number.
+ * @n: the integer to be checked
  *
- *   * @n: the integer to be checked
+ * Return: 1 if prime, otherwise 0
  *
- *    *
- *
- *     * Return: 1 if prime, otherwise 0
- *
- *      */
-
+*/
 int is_prime_number(int n)
-
 {
-
-		if (n == 2)
-
-					return (1);
-
-			else if (n < 2)
-
-						return (0);
-
-				return (_is_prime(n, 2));
-
+if (n == 2)
+return (1);
+else if (n < 2)
+return (0);
+return (_is_prime(n, 2));
 }
-
-
-
 /**
+ * _is_prime - tests for prime
  *
- *  * _is_prime - tests for prime
+ * @n: number to test
  *
- *   * @n: number to test
+ * @i: the counter
  *
- *    * @i: the counter
+ * Return: 1 if prime, otherwise 0
  *
- *     *
- *
- *      * Return: 1 if prime, otherwise 0
- *
- *       */
-
+*/
 int _is_prime(int n, int i)
-
 {
-
-		if (i >= n / 2)
-
-					return (1);
-
-			if (n % i)
-
-						return (_is_prime(n, i + 1));
-
-				return (0);
-
+if (i >= n / 2)
+return (1);
+if (n % i)
+return (_is_prime(n, i + 1));
+return (0);
 }
