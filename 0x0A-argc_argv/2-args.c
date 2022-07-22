3b5104c837d5a787044d8f5699366b57790e5885
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-22 11:56:32 +0300

    betty3

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
-
 /**
+ * main - prints the arguments passed to the prompt
  *
- *  * main - prints the arguments passed to the prompt
+ * @argc: the arguments counter
  *
- *   * @argc: the arguments counter
+ * @argv: the array of passed arguments which includes
  *
- *    * @argv: the array of passed arguments which includes
+ * the program itself
  *
- *     * the program itself
+ * Return: EXIT_SUCCESS
  *
- *      *
- *
- *       * Return: EXIT_SUCCESS
- *
- *        */
-
+*/
 int main(int argc, char *argv[])
-
 {
-
-		while (argc--)
-
-					puts(*argv++);
-
-			return (EXIT_SUCCESS);
-
+while (argc--)
+puts(*argv++);
+return (EXIT_SUCCESS);
 }
