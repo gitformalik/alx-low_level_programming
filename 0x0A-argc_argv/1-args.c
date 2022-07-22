605e41aeb7ff2f1846b26eac37ba65df41e68dc4
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-07-22 11:52:37 +0300

    betty1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
-
 /**
+ * main - prints the number of arguments passed to it.
  *
- *  * main - prints the number of arguments passed to it.
+ * @argc: the size of argv
  *
- *   * @argc: the size of argv
+ * @argv: pointers to an array of characters
  *
- *    * @argv: pointers to an array of characters
+ * Return: EXIT_SUCCESS
  *
- *     *
- *
- *      * Return: EXIT_SUCCESS
- *
- *       */
-
+*/
 int main(int argc, char *argv[] __attribute__((unused)))
-
 {
-
-		printf("%d\n", argc - 1);
-
-			return (EXIT_SUCCESS);
-
+printf("%d\n", argc - 1);
+return (EXIT_SUCCESS);
 }
