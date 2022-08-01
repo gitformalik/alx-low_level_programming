56f96e778bbdd740691e0b862322f764b441872c
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-08-01 23:05:14 +0300

    4th

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,13 @@
 #ifndef DOG
 #define DOG
 /**
- *struct dog - dog class
+ * struct dog - dog class
  *
- *@name: Name of the dog
+ * @name: Name of the dog
  *
- *@age: Age of the dog
+ * @age: Age of the dog
  *
- *@owner: Owner of the dog
+ * @owner: Owner of the dog
  *
 */
 struct dog
