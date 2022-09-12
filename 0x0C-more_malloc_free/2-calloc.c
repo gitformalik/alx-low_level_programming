c31f70731f4f3b749aa83b1f12ed593096056a0a
gitformalik <106790576+gitformalik@users.noreply.github.com>
2022-09-12 19:32:45 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,19 +11,14 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
-	char *ptr;
-
-	if (nmemb == 0 || size == 0)
-		return ('\0');
-
-	ptr = malloc(nmemb * size);
-
-	if (ptr == NULL)
-		return ('\0');
-
-	for (i = 0; i < nmemb * size; i++)
-		ptr[i] = 0;
-
-	return ((void *)ptr);
+unsigned int i;
+char *ptr;
+if (nmemb == 0 || size == 0)
+return ('\0');
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+return ('\0');
+for (i = 0; i < nmemb * size; i++)
+ptr[i] = 0;
+return ((void *)ptr);
 }
